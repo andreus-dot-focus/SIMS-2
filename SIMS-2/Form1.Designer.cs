d616eb3bcea924d7d88158a28a103e7ccfe2e18b
andreus-dot-focus <vorkozhokovandrey@gmail.com>
2021-03-05 15:24:39 +0700

    first commit

--- SIMS-2/Form1.Designer.cs
+++ SIMS-2/Form1.Designer.cs
@@ -30,12 +30,215 @@ namespace SIMS_2
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.runButton = new System.Windows.Forms.Button();
+            this.edSpeed = new System.Windows.Forms.NumericUpDown();
+            this.edHeight = new System.Windows.Forms.NumericUpDown();
+            this.edAngle = new System.Windows.Forms.NumericUpDown();
+            this.speedLabel = new System.Windows.Forms.Label();
+            this.heightLabel = new System.Windows.Forms.Label();
+            this.angleLabel = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timeValueLabel = new System.Windows.Forms.Label();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edSpeed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edAngle)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.stopButton);
+            this.panel1.Controls.Add(this.timeValueLabel);
+            this.panel1.Controls.Add(this.timeLabel);
+            this.panel1.Controls.Add(this.runButton);
+            this.panel1.Controls.Add(this.edSpeed);
+            this.panel1.Controls.Add(this.edHeight);
+            this.panel1.Controls.Add(this.edAngle);
+            this.panel1.Controls.Add(this.speedLabel);
+            this.panel1.Controls.Add(this.heightLabel);
+            this.panel1.Controls.Add(this.angleLabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 101);
+            this.panel1.TabIndex = 1;
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.timeLabel.Location = new System.Drawing.Point(313, 38);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(82, 25);
+            this.timeLabel.TabIndex = 8;
+            this.timeLabel.Text = "Время:";
+            // 
+            // runButton
+            // 
+            this.runButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.runButton.Location = new System.Drawing.Point(598, 11);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(156, 35);
+            this.runButton.TabIndex = 7;
+            this.runButton.Text = "Полетели";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // edSpeed
+            // 
+            this.edSpeed.Location = new System.Drawing.Point(93, 70);
+            this.edSpeed.Name = "edSpeed";
+            this.edSpeed.Size = new System.Drawing.Size(96, 20);
+            this.edSpeed.TabIndex = 6;
+            this.edSpeed.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // edHeight
+            // 
+            this.edHeight.Location = new System.Drawing.Point(93, 43);
+            this.edHeight.Name = "edHeight";
+            this.edHeight.Size = new System.Drawing.Size(96, 20);
+            this.edHeight.TabIndex = 5;
+            // 
+            // edAngle
+            // 
+            this.edAngle.Location = new System.Drawing.Point(93, 14);
+            this.edAngle.Name = "edAngle";
+            this.edAngle.Size = new System.Drawing.Size(96, 20);
+            this.edAngle.TabIndex = 4;
+            this.edAngle.Value = new decimal(new int[] {
+            45,
+            0,
+            0,
+            0});
+            // 
+            // speedLabel
+            // 
+            this.speedLabel.AutoSize = true;
+            this.speedLabel.Location = new System.Drawing.Point(36, 72);
+            this.speedLabel.Name = "speedLabel";
+            this.speedLabel.Size = new System.Drawing.Size(58, 13);
+            this.speedLabel.TabIndex = 2;
+            this.speedLabel.Text = "Скорость:";
+            // 
+            // heightLabel
+            // 
+            this.heightLabel.AutoSize = true;
+            this.heightLabel.Location = new System.Drawing.Point(36, 45);
+            this.heightLabel.Name = "heightLabel";
+            this.heightLabel.Size = new System.Drawing.Size(48, 13);
+            this.heightLabel.TabIndex = 1;
+            this.heightLabel.Text = "Высота:";
+            // 
+            // angleLabel
+            // 
+            this.angleLabel.AutoSize = true;
+            this.angleLabel.Location = new System.Drawing.Point(36, 16);
+            this.angleLabel.Name = "angleLabel";
+            this.angleLabel.Size = new System.Drawing.Size(35, 13);
+            this.angleLabel.TabIndex = 0;
+            this.angleLabel.Text = "Угол:";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.chart1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 99);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 351);
+            this.panel2.TabIndex = 2;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.MarkerBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            series1.MarkerColor = System.Drawing.Color.White;
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(800, 351);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timeValueLabel
+            // 
+            this.timeValueLabel.AutoSize = true;
+            this.timeValueLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.timeValueLabel.Location = new System.Drawing.Point(392, 38);
+            this.timeValueLabel.Name = "timeValueLabel";
+            this.timeValueLabel.Size = new System.Drawing.Size(96, 25);
+            this.timeValueLabel.TabIndex = 9;
+            this.timeValueLabel.Text = "0.00.000";
+            // 
+            // stopButton
+            // 
+            this.stopButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.stopButton.Location = new System.Drawing.Point(598, 55);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(156, 35);
+            this.stopButton.TabIndex = 10;
+            this.stopButton.Text = "Пауза";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edSpeed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edAngle)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label angleLabel;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label speedLabel;
+        private System.Windows.Forms.Label heightLabel;
+        private System.Windows.Forms.NumericUpDown edSpeed;
+        private System.Windows.Forms.NumericUpDown edHeight;
+        private System.Windows.Forms.NumericUpDown edAngle;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label timeValueLabel;
+        private System.Windows.Forms.Button stopButton;
     }
 }
 
--- SIMS-2/Form1.cs
+++ SIMS-2/Form1.cs
@@ -12,9 +12,66 @@ namespace SIMS_2
 {
     public partial class Form1 : Form
     {
+        const double g = 9.81;
+
+        double dt;
+
+        double a;
+        double v0;
+        double y0;
+
+        double t;
+        double x;
+        double y;
+
+        bool isModeling = false;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void runButton_Click(object sender, EventArgs e)
+        {
+            if (isModeling==false)
+            {
+                    dt = (double)timer1.Interval;
+                    a = (double)edAngle.Value;
+                    v0 = (double)edSpeed.Value;
+                    y0 = (double)edHeight.Value;
+
+                    t = 0;
+                    x = 0;
+                    y = y0;
+                    chart1.Series[0].Points.Clear();
+                    chart1.Series[0].Points.AddXY(x, y);
+
+                    double s = v0 * v0 * Math.Sin(2 * a * Math.PI / 180) / g;
+                    double h = (v0 * v0 * Math.Sin(a * Math.PI / 180) * Math.Sin(a * Math.PI / 180)) / (2 * g);
+                    chart1.ChartAreas[0].AxisX.Maximum = s * 1.1;
+                    chart1.ChartAreas[0].AxisY.Maximum = (h + y0) * 1.1;
+
+                    isModeling = true;
+            }
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            t += dt;
+            timeValueLabel.Text = $"{(int)t/60000}.{(int)t/1000}.{(int)t%1000}";
+            x = v0 * Math.Cos(a * Math.PI / 180) * t/1000;
+            y = y0 + v0 * Math.Sin(a * Math.PI / 180) * t/1000 - g * t/1000 * t / 2000;
+            chart1.Series[0].Points.AddXY(x, y);
+            if (y <= 0) 
+            {
+                timer1.Stop();
+                isModeling = false;
+            } 
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
         }
     }
 }
